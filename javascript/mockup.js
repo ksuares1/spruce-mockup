0ad54d49e2d11f1f8d3ed2b1de505e299ff58991
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-06-13 19:51:38 -0400

    updated event background color

---
+++ javascript/mockup.js
@@ -0,0 +1,6 @@
+// var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// function getSliderIndex(slides) {
+//     var currentMonthName = MONTHS[new Date().getMonth()];
+//     return slides.indexOf(currentMonthName)
+// }
